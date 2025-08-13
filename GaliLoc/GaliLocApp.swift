0e7922b2fccd8a9432cee04d7f986f52fd692a38
Nestor Camela <nestorcamela@gmail.com>
2025-08-13 18:57:52 -0300

    Initial Commit

---
+++ GaliLoc/GaliLocApp.swift
@@ -0,0 +1,17 @@
+//
+//  GaliLocApp.swift
+//  GaliLoc
+//
+//  Created by Nestor Camela on 13/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct GaliLocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GaliLocTests/GaliLocTests.swift
@@ -0,0 +1,17 @@
+//
+//  GaliLocTests.swift
+//  GaliLocTests
+//
+//  Created by Nestor Camela on 13/08/2025.
+//
+
+import Testing
+@testable import GaliLoc
+
+struct GaliLocTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
